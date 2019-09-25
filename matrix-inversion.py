123e8375ae301db6737dc6b25d77da24b85a443b
Rick <mistermisterio1000@gmail.com>
2019-09-25 13:17:12 -0600

    FINALLY WORKING

--- matrix-inversion.py
+++ matrix-inversion.py
@@ -39,13 +39,26 @@ I = [
 def main(A, I):
     Am = A
     Im = I
-    tmpA = []
-    tmpI = []
     counter = 0
-    iterate = 0
+    focus = 0
+    focus2 = 0
 
-    for i in list(range(len(Am)))[:counter] + list(range(len(Am)))[counter+1:]:
-        print(Am[i][counter])
+    for i in list(range(len(Am))):
+        focus = Am[counter][counter]
+        for j in list(range(len(Am[i]))):
+            Am[i][j] = round(Am[i][j] / focus, 3)
+            Im[i][j] = round(Im[i][j] / focus, 3)
 
+        for j in list(range(len(Am)))[:counter] + list(range(len(Am)))[counter+1:]:
+            focus2 = Am[j][counter]
+            for k in list(range(len(Am[j]))):
+                Am[j][k] = round(Am[j][k] - Am[counter][k] * focus2, 3)
+                Im[j][k] = round(Im[j][k] - Im[counter][k] * focus2, 3)
+
+        counter += 1
+
+    printmatrix(Am)
+    print()
+    printmatrix(Im)
 
 main(A, I)
